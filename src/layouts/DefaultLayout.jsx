24b3129c06d116301dcab89911ecc657af168af1
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-20 12:29:05 +0200

    [UPD] Create Navbar components

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/header/Navbar";
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default DefaultLayout;
