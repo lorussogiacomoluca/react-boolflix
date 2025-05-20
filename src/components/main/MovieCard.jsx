93d565a8800e916a99e9b7934a2fc74ea8894e2f
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-20 16:17:35 +0200

    [UPD] Converting vote_average in base-5

--- src/components/main/MovieCard.jsx
+++ src/components/main/MovieCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FlagIcon from "./FlagIcon";
 
 const MovieCard = ({ movie }) => {
+  const rating5 = Math.ceil(movie.vote_average / 2);
   return (
     <>
       <div className="card flex-shrink-0" style={{ width: "300px" }}>
@@ -19,6 +20,7 @@ const MovieCard = ({ movie }) => {
             <h5 className="card-title d-flex gap-3">{movie.original_title}</h5>
             <p className="card-text">{movie.title}</p>
             <FlagIcon cod={movie.original_language} />
+            <p className="card-text">Rating5: {rating5}</p>
             <p className="card-text">Rating: {movie.vote_average}</p>
           </div>
         </div>
